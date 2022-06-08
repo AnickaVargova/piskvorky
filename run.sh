a0bb2cba956d1a4898a50bf48cffe61c57058e17
Anna Vargova <vargoa01@Annas-MacBook-Pro.local>
2022-06-08 09:58:49 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-docker build -f Dockerfile -t piskvorky .
+docker build -f Dockerfile -t annavar/piskvorky:3 .
 echo '--------'
-docker run -it -p 80:80 piskvorky
+docker run -it -p 80:80 annavar/piskvorky:3
